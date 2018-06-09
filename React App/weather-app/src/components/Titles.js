71e54c1cb15682f88d9be19268650541b89f9e27
nidhinkumar <nidhinkumar@codingtown.com>
2018-06-09 11:22:38 +0530

    Create Weather App

---
+++ React App/weather-app/src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Titles = () => (
+  <div>
+    <h1>Find Weather</h1>
+    <p>Find out temperature condtions and more..</p>
+  </div>
+);
+
+export default Titles;
